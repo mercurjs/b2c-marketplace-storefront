d16f66018aec13d109ab0570a37c573dae00b55e
pfulara <fulara.pawel@gmail.com>
2025-02-21 08:53:59 +0100

    [MM2-201] - Algolia filtering

--- src/app/[locale]/(main)/categories/[category]/page.tsx
+++ src/app/[locale]/(main)/categories/[category]/page.tsx
@@ -19,28 +19,28 @@ export async function generateMetadata({
   return generateCategoryMetadata(cat)
 }
 
-async function Category({ params }: { params: { category: string } }) {
+function Category({ params }: { params: { category: string } }) {
   const { category: handle } = params
 
-  const category = await getCategoryByHandle([handle])
+  // const category = await getCategoryByHandle([handle])
 
-  const breadcrumbsItems = [
-    {
-      path: category.handle,
-      label: category.name,
-    },
-  ]
+  // const breadcrumbsItems = [
+  //   {
+  //     path: category.handle,
+  //     label: category.name,
+  //   },
+  // ]
 
   return (
     <main className="container">
       <div className="hidden md:block mb-2">
-        <Breadcrumbs items={breadcrumbsItems} />
+        {/* <Breadcrumbs items={breadcrumbsItems} /> */}
       </div>
 
-      <h1 className="heading-xl uppercase">{category.name}</h1>
+      <h1 className="heading-xl uppercase">{handle}</h1>
 
       <Suspense fallback={<ProductListingSkeleton />}>
-        <AlgoliaProductsListing category_id={category.id} />
+        {/* <AlgoliaProductsListing category_id={category.id} /> */}
       </Suspense>
     </main>
   )
