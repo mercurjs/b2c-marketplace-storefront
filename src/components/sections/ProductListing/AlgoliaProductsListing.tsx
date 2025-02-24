c45fead080ad7a7d8719469c9b9231d27b45bb8d
pfulara <fulara.pawel@gmail.com>
2025-02-24 09:57:16 +0100

    algolia filtering fix

--- src/components/sections/ProductListing/AlgoliaProductsListing.tsx
+++ src/components/sections/ProductListing/AlgoliaProductsListing.tsx
@@ -66,7 +66,7 @@ const ProductsListing = () => {
       <div className="text-center w-full my-10">
         <h2 className="uppercase text-primary heading-lg">no results</h2>
         <p className="mt-4 text-lg">
-          Sorry, we can't find any results for your criteria
+          Sorry, we can&apos;t find any results for your criteria
         </p>
       </div>
     )
