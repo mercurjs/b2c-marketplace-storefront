71e7ef5fce47a3aa4c97d5c2d10f783a4bae3ada
mikolvj <mikborowski@gmail.com>
2025-04-24 08:33:03 +0200

    fix: review schema

--- src/components/molecules/ReviewForm/schema.ts
+++ src/components/molecules/ReviewForm/schema.ts
@@ -3,7 +3,10 @@ import { z } from "zod"
 export const reviewSchema = z.object({
   sellerId: z.string(),
   rating: z.number().min(1, "Please rate this product").max(5),
-  opinion: z.string().min(10, "Please write your opinion").max(1000),
+  opinion: z
+    .string()
+    .max(1000, "Opinion must be less than 1000 characters")
+    .optional(),
 })
 
 export type ReviewFormData = z.infer<typeof reviewSchema>
