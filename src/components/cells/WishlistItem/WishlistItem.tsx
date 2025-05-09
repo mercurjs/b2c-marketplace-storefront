f22cc530e27abaede9cfcda7995082b1488a4bf7
pfulara <fulara.pawel@gmail.com>
2025-05-09 12:46:43 +0200

    fix wishlist display

--- src/components/cells/WishlistItem/WishlistItem.tsx
+++ src/components/cells/WishlistItem/WishlistItem.tsx
@@ -27,11 +27,7 @@ export const WishlistItem = ({
   return (
     <div
       className={clsx(
-        "relative group border rounded-sm flex flex-col justify-between p-1 ",
-        {
-          "w-[250px] lg:w-[370px]": true,
-          "w-full h-full": false,
-        }
+        "relative group border rounded-sm flex flex-col justify-between p-1 w-[250px] lg:w-[370px]"
       )}
     >
       <div className="relative w-full h-full bg-primary aspect-square">
@@ -80,39 +76,4 @@ export const WishlistItem = ({
       </Link>
     </div>
   )
-  // return (
-  //   <div className="relative">
-  //     <div className="absolute right-3 top-3 z-10 cursor-pointer">
-  //       <WishlistButton
-  //         productId={product.id}
-  //         wishlist={wishlist}
-  //         user={user}
-  //       />
-  //     </div>
-  //     <Link
-  //       href={`/products/${product.handle}`}
-  //       className="p-1 rounded-sm overflow-hidden max-w-[350px] flex flex-col items-center justify-between h-auto border border-action-primary max-md:w-1/2"
-  //     >
-  //       <div>
-  //         <Image
-  //           src={product.thumbnail || ""}
-  //           alt={product.title}
-  //           width={342}
-  //           height={424}
-  //         />
-  //       </div>
-  //       <div className="flex flex-col gap-1 md:gap-2 p-4 w-full max-w-full h-full justify-between">
-  //         <div className="flex justify-between w-full items-center">
-  //           <div className="flex flex-col w-full">
-  //             <h3 className="heading-sm text-primary">{product.title}</h3>
-  //           </div>
-  //           <p className="px-4 py-2.5 label-sm text-secondary border border-action-primary flex items-center rounded-sm h-fit max-md:hidden">
-  //             3.5
-  //           </p>
-  //         </div>
-  //         <p className="label-lg text-primary">{price}</p>
-  //       </div>
-  //     </Link>
-  //   </div>
-  // )
 }
