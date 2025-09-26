5600c1720d1a2f6d9c4c73864d77054762d2e939
omar nabeel <omar.alkhateeb9919@gmail.com>
2025-09-26 15:01:58 +0300

    proto mobile view

--- src/components/molecules/CountrySelector/CountrySelector.tsx
+++ src/components/molecules/CountrySelector/CountrySelector.tsx
@@ -68,7 +68,7 @@ const CountrySelect = ({ regions }: CountrySelectProps) => {
           defaultValue={
             countryCode
               ? options?.find((o) => o?.country === countryCode)
-              : null
+              : undefined
           }
         >
           <ListboxButton className="relative w-16 flex justify-between items-center h-10 bg-component-secondary text-left  cursor-default focus:outline-none border rounded-lg focus-visible:ring-2 focus-visible:ring-opacity-75 focus-visible:ring-white focus-visible:ring-offset-gray-300 focus-visible:ring-offset-2 focus-visible:border-gray-300 text-base-regular">
